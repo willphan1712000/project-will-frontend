200cc46fdfd72525e99b44a70d6195a5d2c7e8cb
Thanh Nha (Will) Phan <phanthanhnha1712000@gmail.com>
2025-10-09 11:58:41 -0400

    Ready for production, version 1.0.2

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'tsup';
 import cssModulePlugin from "esbuild-plugin-css-module";
  
 export default defineConfig({
-    watch: true,
     format: ['cjs', 'esm'],
     entry: ['./index.ts'],
     dts: true,
