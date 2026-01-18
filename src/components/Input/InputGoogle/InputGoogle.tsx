16a15454cf7c68ec75ad3295e5e74058cfa7be03
Thanh Nha (Will) Phan <phanthanhnha1712000@gmail.com>
2026-01-18 16:14:47 -0500

    Fix styling for Avatar

--- src/components/Input/InputGoogle/InputGoogle.tsx
+++ src/components/Input/InputGoogle/InputGoogle.tsx
@@ -18,6 +18,7 @@ interface Props {
  *
  * @param value value of input
  * @param setValue set value function
+ * @param label set label
  * @param options options object containing focusColor property -> color when input is focused
  *
  * @example
